4683e4b596890bb1d89801957cea7e73c1af2675
nmarforio <n.marforio@gmail.com>
2023-02-14 10:54:17 +0100

    activities state done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import './App.css';
+import "./App.css";
 import Form from "./Components/Form/Form.jsx";
+import useLocalStorageState from "use-local-storage-state";
+import { uid } from "uid";
+import { useState } from "react";
 
 function App() {
+  // const [activities, setActivities] = useLocalStorageState("activitie", {
+  //   defaultvalue: [],
+  // });
+  const [activities, setActivities] = useState([]);
+
+  function handelAddActivity(event) {
+    event.preventDefault();
+
+    const input = document.getElementById("name").value;
+    const checkBox = document.getElementById("isForGoodWeather").checked;
+    setActivities([
+      ...activities,
+      { name: input, isForGoodWeather: checkBox, id: uid() },
+    ]);
+  }
+  console.log(activities);
   return (
     <div className="App">
-      <Form />
+      <Form onAddActivity={handelAddActivity} />
     </div>
   );
 }
--- src/Components/Form/Form.jsx
+++ src/Components/Form/Form.jsx
@@ -1,30 +1,16 @@
-import React from 'react'
+import React from "react";
 
-const Form = () => {
-
-  function onAddActivity(event) {
-    event.preventDefault()
-
-    // const myFormData = new FormData(event.target);
-    // const formDataObj = Object.fromEntries(myFormData.entries());
-    // console.log(formDataObj)
-
-    const input = document.getElementById('name').value
-    const checkBox = document.getElementById('isForGoodWeather').checked
-
-
-  }
-
+const Form = ({ onAddActivity }) => {
   return (
     <form onSubmit={onAddActivity}>
       <h2>Add new Activity</h2>
       <label htmlFor="name">Name:</label>
-      <input id='name' name='name' type="text" />
+      <input id="name" name="name" type="text" />
       <label htmlFor="isForGoodWeather">Good weather Activity</label>
-      <input id='isForGoodWeather' name='isForGoodWeather' type="checkbox" />
+      <input id="isForGoodWeather" name="isForGoodWeather" type="checkbox" />
       <button>Submit</button>
     </form>
-  )
-}
+  );
+};
 
-export default Form
+export default Form;
