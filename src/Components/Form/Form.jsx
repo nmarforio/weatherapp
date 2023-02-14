95083ea4b9907274a5cec0ed442da0e49cb32af9
yoricktf <yorick.tenfeld@gmail.com>
2023-02-14 10:14:54 +0100

    created the form and the onaddactivity function

--- src/Components/Form/Form.jsx
+++ src/Components/Form/Form.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
 
 const Form = () => {
+
+  function onAddActivity(event) {
+    event.preventDefault()
+    console.log('is this working')
+  }
+
   return (
-    <div>Form</div>
+    <form onSubmit={onAddActivity}>
+      <h2>Add new Activity</h2>
+      <label htmlFor="name">Name:</label>
+      <input id='name' name='name' type="text" />
+      <label htmlFor="activityType">Good weather Activity</label>
+      <input id='activityType' name='activityType' type="checkbox" />
+      <button>Submit</button>
+    </form>
   )
 }
 
