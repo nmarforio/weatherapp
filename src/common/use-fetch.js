fc8896d79b8c29ac895f6d5590899dd968a38b1c
nmarforio <n.marforio@gmail.com>
2023-02-14 15:35:34 +0100

    fetch Hooked and some extra styling

---
+++ src/common/use-fetch.js
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react";
+
+export function useFetch(url) {
+  const [isGoodWeather, setIsGoodWeather] = useState({});
+
+  try {
+    useEffect(() => {
+      getAPI();
+      let intervalId = setInterval(getAPI, 5000);
+      return () => {
+        clearInterval(intervalId);
+      };
+    }, []);
+    async function getAPI() {
+      const response = await fetch(url);
+      const data = await response.json();
+      setIsGoodWeather(data);
+      console.log(data);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+  return isGoodWeather;
+}
